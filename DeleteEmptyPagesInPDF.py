73fbea2ca9959928dab5b7d0f8327f94242e3a99
Michał Kwaśniewski <92835884+1NOTME1@users.noreply.github.com>
2024-02-02 12:54:18 +0100

    Update DeleteEmptyPagesInPDF.py

--- DeleteEmptyPagesInPDF.py
+++ DeleteEmptyPagesInPDF.py
@@ -1,28 +1,23 @@
-import fitz  # PyMuPDF
+import fitz
 
 def remove_blank_pages(input_pdf, output_pdf):
-    # Otwórz dokument PDF
+    
     doc = fitz.open(input_pdf)
-    page_numbers = []  # Lista numerów stron do usunięcia
-
-    # Iteruj przez strony i sprawdź, czy są puste
+    page_numbers = []  
+    
     for page_num in range(len(doc)):
         page = doc[page_num]
-        text = page.get_text().strip()  # Pobierz tekst ze strony i usuń białe znaki
-
-        # Jeśli strona jest pusta, dodaj jej numer do listy
+        text = page.get_text().strip()  #
+        
         if not text:
             page_numbers.append(page_num)
 
-    # Usuń strony od końca dokumentu, aby uniknąć błędów indeksowania
     for page_num in reversed(page_numbers):
         doc.delete_page(page_num)
-
-    # Zapisz zmodyfikowany dokument jako nowy plik PDF
+        
     doc.save(output_pdf)
     doc.close()
 
-# Użyj funkcji
-input_pdf = r'C:\Users\m.x\Desktop\x\x.pdf'  # Nazwa pliku wejściowego
-output_pdf = 'output.pdf'  # Nazwa pliku wyjściowego
+input_pdf = r'C:\Users\m.x\Desktop\x\x.pdf' 
+output_pdf = 'output.pdf'
 remove_blank_pages(input_pdf, output_pdf)
